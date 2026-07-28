90a3bad7e408642fac81d6db1ddf947050fa0123
Ahmed220605 <159459773+Ahmed220605@users.noreply.github.com>
2026-07-28 18:25:53 +0500

    built account class in model

---
+++ demo/src/main/java/com/example/demo/model/Account.java
@@ -0,0 +1,59 @@
+package com.example.demo.model;
+
+public class Account {
+    private int accountNo;
+    private String holderName;
+    private String accountType;
+    private double balance;
+
+    public Account(){}
+
+    public Account(int accountNo,String holderName,String accountType,double balance){
+        this.accountNo = accountNo;
+        this.holderName = holderName;
+        this.accountType = accountType;
+        this.balance = balance;
+    }
+
+    public int getAccountNo(){
+        return accountNo;
+    }
+    public void setAccountNo(int accountNo){
+        this.accountNo = accountNo;
+    }
+
+
+    public String getHolderName(){
+        return holderName;
+    }
+    public void setHolderName(String holderName){
+        this.holderName = holderName;
+    }
+
+
+    public String getAccountType(){
+        return accountType;
+    }
+    public void setAccountType(String accountType){
+        this.accountType = accountType;
+    }
+
+
+    public double getBalance(){
+        return balance;
+    }
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
+    @Override
+    public String toString() {
+        return """
+           --------------------------
+           Account No : %d
+           Holder     : %s
+           Type       : %s
+           Balance    : $%.2f
+           --------------------------
+           """.formatted(accountNo, holderName, accountType, balance);
+    }
+}
